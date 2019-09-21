b08ec9f31247fa00558f02d87c6697fbca1a30c5
ArtemAlagizov <alagizov@gmail.com>
2019-09-21 22:12:52 +0200

    added first version

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Instability from './components/unstable-picture/component';
+
+function App() {
+  return (
+    <div className="App">
+      <Instability/>
+    </div>
+  );
+}
+
+export default App;
