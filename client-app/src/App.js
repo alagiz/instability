9064803ca536058fad4caee6451c6c558ddc287f
ArtemAlagizov <alagizov@gmail.com>
2019-09-29 10:04:32 +0200

    testing out travis

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import Instability from './components/unstable-picture/component';
 
 function App() {
-  return (
-    <div className="App">
-      <Instability/>
-    </div>
-  );
+    return (
+        <div className="App">
+            INSTABILITY
+            <Instability/>
+        </div>
+    );
 }
 
 export default App;
