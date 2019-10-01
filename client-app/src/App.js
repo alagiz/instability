27cffcbd142ed808ff0a032c7df0c6cd4c152db3
ArtemAlagizov <alagizov@gmail.com>
2019-10-01 10:39:54 +0200

    testing out travis

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -6,7 +6,7 @@ import Instability from './components/unstable-picture/component';
 function App() {
     return (
         <div className="App">
-            INSTABILITY 93
+            INSTABILITY 94
             <Instability/>
         </div>
     );
