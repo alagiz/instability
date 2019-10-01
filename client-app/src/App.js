04a5a4583cf68820431be862eaf03cc67601da79
ArtemAlagizov <alagizov@gmail.com>
2019-10-01 09:24:52 +0200

    testing out travis ci cd

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -6,7 +6,7 @@ import Instability from './components/unstable-picture/component';
 function App() {
     return (
         <div className="App">
-            INSTABILITY 8
+            INSTABILITY 9
             <Instability/>
         </div>
     );
