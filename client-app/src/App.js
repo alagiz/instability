3f06cdbdee344b62c5defdfa5e7fcdcfc33eb626
Artem Alagizov <alagizov@gmail.com>
2019-10-03 18:25:28 +0200

    updated App.js

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -6,7 +6,7 @@ import Instability from './components/unstable-picture/component';
 function App() {
     return (
         <div className="app">
-            INSTABILITY 98
+            INSTABILITY 97
             <Instability/>
         </div>
     );
